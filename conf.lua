b6c2dbb9dc97fb5866cdf2e97dfd293f20528c20
Izzo <izzoy@wit.edu>
2022-04-04 08:27:05 -0400

    Removed zip and tweaked conf

--- conf.lua
+++ conf.lua
@@ -3,7 +3,7 @@ function love.conf(t)
     t.appendidentity = false            -- Search files in source directory before save directory (boolean)
     t.version = "11.3"                  -- The LÖVE version this game was made for (string)
     t.gammacorrect = false              -- Enable gamma-correct rendering, when supported by the system (boolean)
-    t.console = true
+    --t.console = true
 
     t.window.title = "Ludum Dare 50"         -- The window title (string)
     t.window.icon = nil                 -- Filepath to an image to use as the window's icon (string)
@@ -18,4 +18,11 @@ function love.conf(t)
     t.window.depth = nil                -- The number of bits per sample in the depth buffer
     t.window.stencil = nil              -- The number of bits per sample in the stencil buffer
     t.window.display = 1                -- Index of the monitor to show the window in (number)
+
+    t.modules.data = false               -- Enable the data module (boolean)
+    t.modules.mouse = false              -- Enable the mouse module (boolean)
+    t.modules.physics = false            -- Enable the physics module (boolean)
+    t.modules.thread = false             -- Enable the thread module (boolean)
+    t.modules.touch = false              -- Enable the touch module (boolean)
+    t.modules.video = false              -- Enable the video module (boolean)
 end
